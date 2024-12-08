167251c631e9574a4db82a63331583e4077d4224
Michael <michaelsb065@gmail.com>
2024-12-08 01:48:06 -0300

    Correções

--- gerenciador_tarefas.py
+++ gerenciador_tarefas.py
@@ -1,12 +1,16 @@
 def linhas():
     print('-=' * 40)
 
+
 class Tarefa:
-    def __init__(self, titulo, descricao):
-        self.titulo = titulo
+    def __init__(self, nome, descricao):
+        self.nome = nome
         self.descricao = descricao
-        self.status_tarefa = False
-        
+        self.status_tarefa = False        
+
+
+    def __str__(self):
+        return f'Tarefa: {self.nome}. Descrição: {self.descricao}'
 
 
 class GerenciadorTarefa:
@@ -15,22 +19,42 @@ class GerenciadorTarefa:
 
 
     def adicionar_tarefa(self):
-        linhas()
-        print('Adicionar tarefa: ')
-        linhas()
+        descricao = None
 
         while True:
-            nome_tarefa = Tarefa(self.titulo)
-            descricao_tarefa = Tarefa(self.descricao)
-            self.tarefas.append(nome_tarefa)
-            self.tarefas.append(descricao_tarefa)
+            linhas()
+            print('Adicionar tarefa:')
 
-            opcao = input('Deseja adicionar outra tarefa? [Sim/Não]: ').strip().lower()
+            adicionar_tarefa = input('Você deseja adicionar alguma tarefa: [Sim/Não]: ').strip().lower()
+            print()
 
-            if opcao != 'sim':
+            if adicionar_tarefa == 'sim':
+                nome_tarefa = input('Tarefa: ').strip().lower()
+                if nome_tarefa not in [tarefa.nome for tarefa in self.tarefas]:
+                    adicionar_descricao = input('Deseja adicionar uma descrição? [Sim/Não]: ').strip().lower()
+                    if adicionar_descricao == 'sim':
+                        descricao = input('Descrição da tarefa: ')    
+                    nova_tarefa = Tarefa(nome_tarefa, descricao)
+                    self.tarefas.append(nova_tarefa)
+                    print(f'\033[32mSucesso\033[0m. Tarefa {nome_tarefa} adicionada com sucesso. ')
+                else:
+                    print(f'Tarefa {nome_tarefa} já está na lista de tarefas. ')
+            elif adicionar_tarefa in ['nao', 'não']:
                 break
+            else:
+                print('\033[31mERRO\033[0m: Opção invalída. ')
+                continue
 
-tarefa = Tarefa()
+
+    def exibir_tarefas(self):
+        if not self.tarefas:
+            print('Nenhuma tarefa foi adicionada.')
+        else:
+            for tarefa in self.tarefas:
+                print(tarefa)
+
+
 gerenciador = GerenciadorTarefa()
 
-gerenciador.adicionar_tarefa(tarefa)
+gerenciador.adicionar_tarefa()
+gerenciador.exibir_tarefas()
